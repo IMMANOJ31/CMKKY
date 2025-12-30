637a1686c152741d758a60b27e570e1b06d1b1af
IMMANOJ31 <manojkumard.21is@saividya.ac.in>
2025-12-30 11:04:18 +0530

    basic concepts

---
+++ src/main/java/threads/MyRunnable.java
@@ -0,0 +1,13 @@
+package threads;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Overridden method");
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MyRunnable());
+        thread.start();
+    }
+}
